b06076c2b5f4c47ba1868f4cc4674d3a897f8040
Kosta Taneski <kosta@taneski.com>
2021-12-16 11:26:38 +0100

    day 16

---
+++ day16.py
@@ -0,0 +1,90 @@
+import math
+
+with open('day16-t.txt', 'r') as file:
+  lines = file.read().strip()
+
+p = bin(int(lines, 16))[2:].zfill(len(lines)*4)
+
+def get_num(p, s):
+  ret = ""
+  pointer = s
+  while p[pointer] == "1":
+    ret += p[pointer+1:pointer+5]
+    pointer += 5
+  ret += p[pointer+1:pointer+5]
+  return pointer+5, int(ret, 2)
+
+def parse_eq(typ, arr):
+  if typ == 0:
+    return(sum(arr))
+  elif typ == 1: 
+    return(math.prod(arr))
+  elif typ == 2: 
+    return(min(arr))
+  elif typ == 3: 
+    return(max(arr))
+  elif typ == 5:
+    if arr[0] > arr[1]:
+      return 1
+    else:
+      return 0
+  elif typ == 6:
+    if arr[0] < arr[1]:
+      return 1
+    else:
+      return 0
+  elif typ == 7:
+    if arr[0] == arr[1]:
+      return 1
+    else:
+      return 0
+
+ver_sum = 0
+
+def parse(p, start, l):
+  pointer = start
+  global ver_sum
+
+  ver = int(p[pointer:pointer+3], 2)
+  ver_sum += ver
+  pointer += 3 
+
+  typ =  int(p[pointer:pointer+3], 2)
+  pointer += 3 
+
+  if typ == 4:
+    pointer, num = get_num(p, pointer)
+    return pointer, num
+
+  elif typ != 4:
+    lti = int(p[pointer:pointer+1])
+    pointer += 1
+
+    if lti == 0:
+      ln = int(p[pointer:pointer+15], 2)
+      pointer += 15
+      arr = []
+      read_ln = pointer + ln
+
+      while True:
+        pointer, num = parse(p, pointer, l+1)
+        if num is not None:
+          arr.append(num)
+        if pointer == read_ln: 
+          break
+      return pointer, parse_eq(typ, arr)
+
+    elif lti == 1:
+      ln = int(p[pointer:pointer+11], 2)
+      pointer += 11
+      arr = []
+
+      for _ in range(ln):
+        pointer, num = parse(p, pointer, l+1)
+        if num is not None:
+          arr.append(num)
+      return pointer, parse_eq(typ, arr)
+
+pointer, out = parse(p, 0, 0)
+print(ver_sum)
+print(out)
