abbb2931f778723fc22fe403f880ea87655ef2df
Kosta Taneski <kosta@taneski.com>
2022-12-20 22:49:40 +0100

    day20

---
+++ 2022/day20/day20-p1.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+with open('day20.txt', 'r') as file:
+    numbers = [int(l.strip()) for l in file.readlines()]
+
+L = deque(enumerate(numbers))
+
+for i in range(len(L)):
+    while L[0][0] != i:
+        L.append(L.popleft())
+
+    if L[0][1] != 0:
+
+        v = L.popleft()
+        offset = v[1]
+        
+        # move left
+        if offset < 0:
+            for i in range(abs(offset)%len(L)):
+                L.appendleft(L.pop())
+            L.append(v)
+
+        # move right
+        if offset > 0:
+            for i in range(abs(offset)%len(L)):
+                L.append(L.popleft())
+            L.appendleft(v)
+
+while L[0][1] != 0:
+    L.append(L.popleft())
+
+print(L[1000%len(L)][1] + L[2000%len(L)][1] + L[3000%len(L)][1])
