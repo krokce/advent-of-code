efabada4c98bebeda41b2e6353babe20a4bfe280
Kostadin Taneski <kosta@taneski.com>
2023-12-18 19:55:10 +0100

    simplify day 12

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,14 +1,16 @@
-from functools import cache
-
 with open('day12.txt', 'r') as file:
 	lines = [l.strip().split() for l in file.readlines()]
 
-@cache
-def get_count(record, cgroups, in_group_count):
+D = {}
+
+def get_count(record, contiguous_group, damaged_segments):
 	cnt = 0
 
+	if (record, contiguous_group, damaged_segments) in D:
+		return D[(record, contiguous_group, damaged_segments)]
+	
 	# no more groups to fill
-	if len(cgroups) == 0:
+	if len(contiguous_group) == 0:
 		if '#' not in record:
 			return 1
 		else:
@@ -16,30 +18,30 @@ def get_count(record, cgroups, in_group_count):
 	
 	# end of record
 	if len(record) == 0:
-		if len(cgroups) == 1 and cgroups[0] == in_group_count:
+		if len(contiguous_group) == 1 and contiguous_group[0] == damaged_segments:
 			return 1
 		else:
 			return 0
 
-	# impossible to fit remaining groups in the rest of the record from this position
-	if len(record) + in_group_count < sum(cgroups) + len(cgroups) - 1:
-		return 0
+	# cgroup count fits counted damaged segments, 
+	# start new cgroup aand reset damaged_segments counter
+	if damaged_segments == contiguous_group[0]:
+		if record[0] != '#':
+			cnt += get_count(record[1:], contiguous_group[1:], 0)
+	
+	# damaged segments count in progress
+	if damaged_segments > 0:
+		if record[0] in '#?':
+			cnt += get_count(record[1:], contiguous_group, damaged_segments+1)
+	
+	# not in cgroup, start new damaged segments counter or/and skip character
+	if damaged_segments == 0:
+		if record[0] in '#?':
+			cnt += get_count(record[1:], contiguous_group, 1)
+		if record[0] in '.?':
+			cnt += get_count(record[1:], contiguous_group, 0)
 
-	if in_group_count > 0:
-		# group count exausted
-		if in_group_count == cgroups[0]:
-			if record[0] != '#':
-				cnt += get_count(record[1:], cgroups[1:], 0)
-		else:
-			if record[0] in '#?':
-				cnt += get_count(record[1:], cgroups, in_group_count+1)
-	else:
-		if record[0] == '#':
-			cnt += get_count(record[1:], cgroups, in_group_count+1)
-		elif record[0] == '.':
-			cnt += get_count(record[1:], cgroups, 0)
-		elif record[0] == '?':
-			cnt += get_count(record[1:], cgroups, in_group_count+1) + get_count(record[1:], cgroups, 0)
+	D[(record, contiguous_group, damaged_segments)] = cnt
 	return cnt
 
 #1
@@ -55,10 +57,8 @@ ret = 0
 for l in lines:
 	record = l[0]
 	cgroup = list(map(int, l[1].split(',')))
-
 	record = '?'.join([record] * 5)
 	cgroup = tuple(cgroup * 5)
-
 	ret += get_count(record, cgroup, 0)
 
 print(ret)
