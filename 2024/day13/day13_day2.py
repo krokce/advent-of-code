d283ea46c3599c43d4715535961bfac27409afa3
Kostadin Taneski <kosta@taneski.com>
2024-12-14 13:54:35 +0100

    day 13

---
+++ 2024/day13/day13_day2.py
@@ -0,0 +1,19 @@
+import re
+from z3 import *
+
+with open('input.txt', 'r') as file:
+	lines = file.read()
+
+ret = 0
+for l in lines.split("\n\n"):
+	ax, ay, bx, by, px, py = list(map(int, re.findall(r'\d+', l)))
+	a, b = Ints("a b")
+	s = Solver()
+	s.add(a * ax + b * bx == px + 10000000000000)
+	s.add(a * ay + b * by == py + 10000000000000)
+	result = s.check()
+	if result == sat:
+		m = s.model()
+		ret = ret + m[a].as_long()*3 + m[b].as_long()
+
+print(ret)
